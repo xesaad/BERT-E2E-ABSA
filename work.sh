8e5c45fecdfd67d7269c674b482b5c1d92a83028
Alex Saad <asaad@wayfair.com>
2022-04-29 16:47:46 +0200

    Allow inference to run with a different example

--- work.sh
+++ work.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-TASK_NAME="test20"
-ABSA_HOME="./bert-linear-laptop14-finetune"
+TASK_NAME="random_samples_lapt14"
+ABSA_HOME="./bert-san-random_samples_lapt14-finetune"
 CUDA_VISIBLE_DEVICES=0 python work.py --absa_home ${ABSA_HOME} \
-                      --ckpt ${ABSA_HOME}/checkpoint-1500 \
+                      --ckpt ${ABSA_HOME}/checkpoint-1400 \
                       --model_type bert \
                       --data_dir ./data/${TASK_NAME} \
                       --task_name ${TASK_NAME} \
-                      --model_name_or_path bert-base-uncased \
+                      --model_name_or_path bert-large-uncased \
                       --cache_dir ./cache \
                       --max_seq_length 128 \
                       --tagging_schema BIEOS
